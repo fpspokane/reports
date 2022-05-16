e98a41648d45517ed34535e5042b7ad1948bd602
grant.wylie <grant@edgesource.net>
2022-05-15 21:59:38 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-chartjs-example';
+  chartData = [
+    {
+      data: [330, 600, 260, 700],
+      label: 'Account A'
+    },
+    {
+      data: [120, 455, 100, 340],
+      label: 'Account B'
+    },
+    {
+      data: [45, 67, 800, 500],
+      label: 'Account C'
+    }
+  ];
+
+  chartLabels = [
+    'January',
+    'February',
+    'March',
+    'April'
+  ];
+
+  chartOptions = {
+    responsive: true
+  };
+
 }
