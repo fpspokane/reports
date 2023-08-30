d9699afcf4335d1a2af8af4339aa9c645325bfc2
grant.wylie <grant@edgesource.net>
2023-08-30 00:16:10 -0400

    update reports page to use component, add reports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,31 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-chartjs-example';
-  chartData = [
-    {
-      data: [330, 600, 260, 700],
-      label: 'Account A'
-    },
-    {
-      data: [120, 455, 100, 340],
-      label: 'Account B'
-    },
-    {
-      data: [45, 67, 800, 500],
-      label: 'Account C'
-    }
-  ];
+  openEsStats() {
+    const url1 = 'https://app.powerbi.com/view?r=eyJrIjoiMzRjMDk5MTktZGZmOC00MWJmLTliZjgtMzE4MTUwNjA4Mzc3IiwidCI6IjJjM2U0MzY5LTAxNjMtNDE5Zi05NGY1LTZlZTdkNTIwZWRkZiJ9'
+    window.open(url1, "name1");
+  }
 
-  chartLabels = [
-    'January',
-    'February',
-    'March',
-    'April'
-  ];
+  openAllProjectsStats() {
+    const url2 = 'https://app.powerbi.com/view?r=eyJrIjoiYmJjNGFmNTktYTIzYi00MDU2LTg5OWQtMmE3MGIwMDI1ZDBiIiwidCI6IjJjM2U0MzY5LTAxNjMtNDE5Zi05NGY1LTZlZTdkNTIwZWRkZiJ9'
+    window.open(url2, "name2");
+  }
 
-  chartOptions = {
-    responsive: true
-  };
-
+  openFamPromPreventionStats() {
+    const url3 = 'https://app.powerbi.com/view?r=eyJrIjoiNTVhYWI2MjMtOTI5Mi00MWFhLWFkODYtYjhhMDA0ZTBjMTNhIiwidCI6IjJjM2U0MzY5LTAxNjMtNDE5Zi05NGY1LTZlZTdkNTIwZWRkZiJ9'
+    window.open(url3, "name3");
+  }
 }
