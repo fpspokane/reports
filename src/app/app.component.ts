d272133394624571405a6e78442864111b50cf4e
troycaselli <swordandtrowel1689@gmail.com>
2023-09-08 10:29:27 -0700

    moved app content to splash-page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,22 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  openEsStats() {
-    const url1 =
-      'https://app.powerbi.com/view?r=eyJrIjoiMzRjMDk5MTktZGZmOC00MWJmLTliZjgtMzE4MTUwNjA4Mzc3IiwidCI6IjJjM2U0MzY5LTAxNjMtNDE5Zi05NGY1LTZlZTdkNTIwZWRkZiJ9';
-    window.open(url1, 'name1');
-  }
-
-  openAllProjectsStats() {
-    const url2 =
-      'https://app.powerbi.com/view?r=eyJrIjoiYmJjNGFmNTktYTIzYi00MDU2LTg5OWQtMmE3MGIwMDI1ZDBiIiwidCI6IjJjM2U0MzY5LTAxNjMtNDE5Zi05NGY1LTZlZTdkNTIwZWRkZiJ9';
-    window.open(url2, 'name2');
-  }
-
-  openFamPromPreventionStats() {
-    const url3 =
-      'https://app.powerbi.com/view?r=eyJrIjoiNTVhYWI2MjMtOTI5Mi00MWFhLWFkODYtYjhhMDA0ZTBjMTNhIiwidCI6IjJjM2U0MzY5LTAxNjMtNDE5Zi05NGY1LTZlZTdkNTIwZWRkZiJ9';
-    window.open(url3, 'name3');
-  }
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ChartsModule } from 'ng2-charts';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { SplashPageComponent } from './splash-page/splash-page.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SplashPageComponent],
   imports: [BrowserModule, ChartsModule],
   providers: [],
   bootstrap: [AppComponent],
