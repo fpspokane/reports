c758c1af2e8cbd79078624fe343ceda10e5f5a9b
troycaselli <swordandtrowel1689@gmail.com>
2023-09-08 11:13:28 -0700

    set up router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { SplashPageComponent } from './splash-page/splash-page.component';
 
+const appRoutes: Routes = [{ path: '', component: SplashPageComponent }];
+
 @NgModule({
   declarations: [AppComponent, SplashPageComponent],
-  imports: [BrowserModule, ChartsModule],
+  imports: [
+    BrowserModule,
+    ChartsModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
