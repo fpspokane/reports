3d1b97e12fc8381f4aaa555d6179fd67e5ae3e7d
troycaselli <swordandtrowel1689@gmail.com>
2023-09-08 18:12:04 -0700

    initialized components and added new logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,19 @@ import { ChartsModule } from 'ng2-charts';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { SplashPageComponent } from './splash-page/splash-page.component';
+import { SplashPageComponent } from './components/splash-page/splash-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
 const appRoutes: Routes = [{ path: '', component: SplashPageComponent }];
 
 @NgModule({
-  declarations: [AppComponent, SplashPageComponent],
+  declarations: [
+    AppComponent,
+    SplashPageComponent,
+    NavbarComponent,
+    HomeComponent,
+  ],
   imports: [
     BrowserModule,
     ChartsModule,
