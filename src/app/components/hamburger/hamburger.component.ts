6437ec55358cbf5e1b0b7936fd2892a87e95c5a4
troycaselli <swordandtrowel1689@gmail.com>
2023-09-27 16:02:27 -0700

    set up hamburger component toggle

--- src/app/components/hamburger/hamburger.component.ts
+++ src/app/components/hamburger/hamburger.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-hamburger',
   templateUrl: './hamburger.component.html',
-  styleUrls: ['./hamburger.component.css']
+  styleUrls: ['./hamburger.component.css'],
 })
 export class HamburgerComponent implements OnInit {
+  @Input() data!: boolean;
+  @Input() toggleHamburger!: Function;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
